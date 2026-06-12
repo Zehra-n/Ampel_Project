e23002f5467f89e9bb99d4daa5f5c0ce54372af2
Zehra_Nuhiu <zehra_nuhiu@sluz.ch>
2026-06-12 14:46:14 +0200

    feat: add TrafficLight class with pin setup and transition methods

---
+++ traffic_light.py
@@ -0,0 +1,47 @@
+from machine import Pin
+from utime import sleep
+
+TRANSITION_DELAY = 1
+
+class TrafficLight:
+    def __init__(self, pin_red, pin_yellow, pin_green):
+        self.red = Pin(pin_red, Pin.OUT)
+        self.yellow = Pin(pin_yellow, Pin.OUT)
+        self.green = Pin(pin_green, Pin.OUT)
+        self.all_off()
+
+    def all_off(self):
+        self.red.off()
+        self.yellow.off()
+        self.green.off()
+
+    def set_red(self):
+        self.all_off()
+        self.red.on()
+
+    def set_red_yellow(self):
+        self.all_off()
+        self.red.on()
+        self.yellow.on()
+
+    def set_green(self):
+        self.all_off()
+        self.green.on()
+
+    def set_yellow(self):
+        self.all_off()
+        self.yellow.on()
+
+    def transition_to_green(self):
+        self.set_red()
+        sleep(TRANSITION_DELAY)
+        self.set_red_yellow()
+        sleep(TRANSITION_DELAY)
+        self.set_green()
+
+    def transition_to_red(self):
+        self.set_green()
+        sleep(TRANSITION_DELAY)
+        self.set_yellow()
+        sleep(TRANSITION_DELAY)
+        self.set_red()
